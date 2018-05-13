127908240ea5f4c8d830affff168df06369695fe
muturi254 <petermuturi74@gmail.com>
2018-05-13 13:47:49 +0300

    updated settings with bootstra 4

--- instagram/settings.py
+++ instagram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=($fu2g4uy=xb@h5-!iv%tt_q%#fjey-9crgj7j(mkvn=07hpg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
     'insta.apps.InstaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -78,8 +79,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'ashorta',
+        'PASSWORD': 'Number12',
     }
 }
 
