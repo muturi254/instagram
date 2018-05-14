46284309afdaf0230710ca0654cf9ddd13fb431d
muturi254 <petermuturi74@gmail.com>
2018-05-14 05:18:28 +0300

    model logic

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -6,20 +10,26 @@ class Profile(models.Model):
         ('male', 'Male'),
         ('female', 'Female')
     )
-    profile_icon = models.ImageField(upload_to='profile')
+    profile_icon = ProcessedImageField(upload_to='avatars', processors=[ResizeToFill(100, 50)],format='JPEG', options={'quality': 60})
     bio = models.TextField()
     username = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     gender = models.CharField(max_length=6, choices=GENDER_CHOICE, default='None')
 
 class Comments(models.Model):
-    # comentor = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentor = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
 
-    pass
+class Likes(models.Model):
+    user_like = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 
 class Image(models.Model):
     image = models.ImageField(upload_to='uploads/')
     image_name = models.CharField(max_length=30)
     image_caption = models.TextField()
-    # image_likes = models.ForeignKey(Profile)
-    image_comments = models.ForeignKey(Comments)
-    # image_owner = models.ForeignKey(Profile)
+    image_likes = models.ForeignKey(Likes, on_delete=models.CASCADE)
+    image_comments = models.ForeignKey(Comments, on_delete=models.CASCADE)
+    image_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    upload_date = models.DateField(auto_now_add=True)
+
