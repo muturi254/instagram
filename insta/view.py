edcda523dde3e30e0f77585e98886365d3c8ef1f
muturi254 <petermuturi74@gmail.com>
2018-05-14 05:15:28 +0300

    added profile view

--- insta/view.py
+++ insta/view.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+# from django.http import HttpResponse
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render(request,'index.html')
+
+@login_required(login_url='/accounts/login/')
+def profile(request, question_id):
+    user_name = request.user.username
+    print(user_name)
+    return render(request, 'profile.html')
