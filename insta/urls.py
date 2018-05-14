7f62a10a86fb2b582e5f90f6e04acb768ddeba35
muturi254 <petermuturi74@gmail.com>
2018-05-14 05:19:22 +0300

    trying out predifined variable on url pattern

--- insta/urls.py
+++ insta/urls.py
@@ -4,6 +4,7 @@ from . import view
 
 urlpatterns = [
     url(r'^$', view.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', view.profile, name = 'profile'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
