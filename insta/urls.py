aa471e14fcbabe8d8ef2efe400fcb727ed78114a
muturi254 <petermuturi74@gmail.com>
2018-05-15 11:30:18 +0300

    url for post

--- insta/urls.py
+++ insta/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib.auth import views
 from . import view
@@ -6,6 +8,9 @@ urlpatterns = [
     url(r'^$', view.index, name='index'),
     url(r'^(?P<user_id>[0-9]+)/$', view.profile, name = 'profile'),
     url(r'^(?P<user_id>[0-9]+)/edits/$', view.profile_edit, name='profile_edit'),
+    url(r'^Post/New/$', view.post, name='post'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
