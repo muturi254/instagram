0248c30c09cb4d75be757a622e92b0ffd0a51c0d
muturi254 <petermuturi74@gmail.com>
2018-05-15 20:04:30 +0300

    like view

--- insta/view.py
+++ insta/view.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from .forms import ProfileForm, profileEdit, ImageUpload
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from .forms import ProfileForm, profileEdit, ImageUpload, CommentForm
 from django.contrib import messages
-from .models import Image
-
+from .models import Image, Comments
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
@@ -68,9 +68,64 @@ def post(request):
         form = ImageUpload(request.POST, request.FILES)
         if form.is_valid():
             photo = form.save(commit=False)
-            photo.user = current_user
+            photo.image_owner = current_user
             photo.save()
             return redirect('index')
     else:
         form = ImageUpload()
     return render(request, 'post.html', {"form": form})
+
+
+@login_required(login_url='/accounts/register')
+def like(request):
+    '''
+    The view starts by looking for a GET variable called id. If it finds one, it retrieves the
+    Image object that is associated with this id.
+    Next, the view checks to see whether the user has voted for this bookmark before.
+    This is done by calling the filter method.
+    If this is the first time that the user has liked for this bookmark, we increment the
+    post.likes
+    '''
+    if request.GET['id']:
+        try:
+            id = request.GET['id']
+            post = Image.objects.get(id=id)
+            user_liked = post.users_liked.filter(
+                username=request.user.username)
+            if not user_liked:
+                post.image_likes += 1
+                post.users_liked.add(request.user)
+                post.save()
+
+            elif user_liked and post.image_likes != 0:
+                post.image_likes -= 1
+                post.users_liked.remove(request.user)
+                post.save()
+        except ObjectDoesNotExist:
+            raise Http404('Post not found.')
+
+    if request.META['HTTP_REFERER']:
+        return HttpResponseRedirect(request.META['HTTP_REFERER'], {"user_liked": user_liked})
+
+    return HttpResponseRedirect('/')
+
+@login_required(login_url='/accounts/login/')
+def comment(request, id):
+    current_user = request.user
+    image = Image.objects.get(pk=id)
+    commenty = Comments.objects.all()
+    print(current_user.profile.avatar)
+
+    print(image)  
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.writer = current_user
+            comment.post = image
+            comment.save()
+            return redirect('index')
+    else:
+        form = CommentForm()
+
+    return render(request, 'comment.html', {'form':form, 'image':image, 'commenty':commenty })
