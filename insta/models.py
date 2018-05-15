ae2c1382a9f873bf8fe788daa376e4bebc53755f
muturi254 <petermuturi74@gmail.com>
2018-05-15 11:24:31 +0300

    models image

--- insta/models.py
+++ insta/models.py
@@ -1,35 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
 
-# Create your models here.
 class Profile(models.Model):
-    GENDER_CHOICE =(
-        ('male', 'Male'),
-        ('female', 'Female')
-    )
-    profile_icon = ProcessedImageField(upload_to='avatars', processors=[ResizeToFill(100, 50)],format='JPEG', options={'quality': 60})
-    bio = models.TextField()
-    username = models.CharField(max_length=20)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    gender = models.CharField(max_length=6, choices=GENDER_CHOICE, default='None')
-
-class Comments(models.Model):
-    comentor = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.TextField()
-
-class Likes(models.Model):
-    user_like = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # avatar_thumbnail = ProcessedImageField(upload_to='avatars',processors=[ResizeToFill(100, 50)],format='JPEG', options={'quality': 60})
+    avatar = models.ImageField(upload_to='avatar', blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateTimeField(null=True, blank=True)
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='uploads/')
+    image = models.ImageField(upload_to='post/')
     image_name = models.CharField(max_length=30)
     image_caption = models.TextField()
-    image_likes = models.ForeignKey(Likes, on_delete=models.CASCADE)
-    image_comments = models.ForeignKey(Comments, on_delete=models.CASCADE)
-    image_owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    upload_date = models.DateField(auto_now_add=True)
+    # image_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
